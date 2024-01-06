e1f2fad436fe084a7d2f35a2ab69cef5b36742fe
Khushi Joshi <155726148+KhushiJoshi03@users.noreply.github.com>
2024-01-06 11:04:48 +0530

    Add files via upload

--- Category.js
+++ Category.js
@@ -1,60 +1,64 @@
 import axios from "axios";
 import React from "react";
-class Category extends React.Component
-{
-    constructor(props)
-    {
+class Category extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            mydata:[],
-            isload:false
+        this.state = {
+            mydata: [],
+            isload: false
         }
-        
+
     }
-    componentDidMount()
-    {
+    componentDidMount() {
         axios.get("https://akashsir.in/myapi/atecom1/api/api-view-category.php")
-        .then((res)=>{
-            console.log(res.data.category_list)
-            this.setState({
-                mydata:res.data.category_list,
-                isload:true
+            .then((res) => {
+                console.log(res.data.category_list)
+                this.setState({
+                    mydata: res.data.category_list,
+                    isload: true
+                })
             })
-        })
     }
-    render()
-        {
-            var {isload,mydata}=this.state
-            if(!isload)
-            {
-                return <h1>Loading...</h1>
-            }
-            return(
-                <>
-                <table border={1}>
-                <thead>
-                        <tr>
-                            <td>Id</td>
-                            <td>Name</td>
-                            <td>Image</td>
-                        </tr>
-                    </thead>
-                {mydata.map((v)=>{
-                    return(
-                        
-                        <tbody>
-                            <tr>
-                                <td>{v.category_id}</td>
-                                <td>{v.category_name}</td>
-                               <td><img src={v.category_image} width={200}/></td>
-                            </tr>
-                        </tbody>
-                    
-                    )
-                })}
-                </table>
-                </>
-            )
+    render() {
+        var { isload, mydata } = this.state
+        if (!isload) {
+            return <h1>Loading...</h1>
         }
+        return (
+            <>  
+                <div class="container">
+                <div class="row product" >
+                    {mydata.map((v) => {
+                        return (
+                            <>
+
+                                <div class="col-md-4">
+                                    <section class="articles">
+                                        <article>
+                                            <div class="article-wrapper">
+                                                <figure>
+                                                    <img src={v.category_image} alt="" />
+                                                </figure>
+                                                <div class="article-body">
+                                                    <h2>{v.category_name}</h2>
+                                                    <a class="btn btn-primary">Buy Now</a>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </section>
+
+                                </div>
+
+
+
+                            </>
+                        )
+                    })}
+                </div>
+                </div>
+
+            </>
+        )
+    }
 }
 export default Category;
